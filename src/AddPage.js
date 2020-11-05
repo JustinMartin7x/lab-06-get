5e4580669d72147f7f5ce640372b6b8038de6aab
JustinMartin7x <justin.martin7x@gmail.com>
2020-11-05 10:03:53 -0800

    restructured and added ADDPAGE

---
+++ src/AddPage.js
@@ -0,0 +1,103 @@
+import React, { Component } from 'react'
+import fetch from 'superagent'
+
+const localStorageUser = {
+    userId: 1
+}
+
+export default class AddPage extends Component {
+    state = {
+        classes: []
+    }
+    componentDidMount = async () => {
+        const data = await fetch.get('https://radiant-ridge-10683.herokuapp.com/classes');
+        this.setState({
+            classes: data.body
+        })
+    }
+    handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const newShip = {
+            name: this.state.name,
+            weapons: this.state.weapons,
+            docked: this.state.weapons,
+            size: this.state.size,
+            class_id: this.state.class_id,
+            image: this.state.image,
+            owner_id: localStorageUser.userId,
+        }
+        await fetch
+            .post('https://radiant-ridge-10683.herokuapp.com/ships')
+            .send(newShip)
+    }
+    handleName = (e) => {
+        this.setState({
+            name: e.target.value
+        })
+
+    }
+    handleWeapons = (e) => {
+        this.setState({
+            weapons: e.target.value
+        })
+
+    }
+    handleDocked = (e) => {
+        this.setState({
+            docked: e.target.value
+        })
+
+    }
+    handleSize = (e) => {
+        this.setState({
+            size: e.target.value
+        })
+
+    }
+    handleClass = (e) => {
+        this.setState({
+            class_id: e.target.value
+        })
+
+    }
+    handleImage = (e) => {
+        this.setState({
+            image: e.target.value
+        })
+
+    }
+    render() {
+        return (
+            <div>
+                <form onSubmit={this.handleSubmit}>
+                    Name<input onChange={this.handleName}></input>
+                weapons Number<input onChange={this.handleWeapons}></input>
+                Docked <select onChange={this.handleDocked}>
+                        <option value="true">yes</option>
+                        <option value="false">no</option>
+                    </select>
+                size <select onChange={this.handleSize}>
+                        <option value="small">small</option>
+                        <option value="medium">Medium</option>
+                        <option value="large">Large</option>
+                        <option value="very large">Very Large</option>
+                        <option value="massive">Massive</option>
+                    </select>
+                Ship Class <select onChange={this.handleClass}>
+                        {
+                            this.state.classes.map((item, key) =>
+                                <option value={item.id}>{item.name}</option>
+                            )
+                        }
+                    </select>
+                Image <input onChange={this.handleImage}></input>
+                    <button>submit</button>
+                </form>
+
+
+
+            </div>
+        )
+    }
+}
---
+++ src/ListPage.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import fetch from 'superagent'
+
+export default class App extends Component {
+    state = {
+        objects: [],
+        loading: true
+    }
+
+
+
+    componentDidMount = async () => {
+        this.setState({
+            loading: true
+        })
+        const data = await fetch.get('https://radiant-ridge-10683.herokuapp.com/ships')
+        this.setState({
+            objects: data.body,
+            loading: false
+        })
+        console.log(this.state.objects)
+    }
+
+    render() {
+        return (
+            <div >
+                {
+                    !this.state.loading ?
+
+                        this.state.objects.map((data, i) =>
+                            <div>
+                                <img src={data.image} alt="ship" />
+                                <h1>{data.name}</h1>
+                                <h3>{data.size}</h3>
+                                <h3>{data.class}</h3>
+                                <h3>{data.weapons}</h3>
+
+
+                            </div>
+                        ) :
+                        "Loading"
+
+                }
+
+
+
+            </div>
+        );
+    }
+}
